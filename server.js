ccdd2af7b1d9a68f2bbf77b4f5bf93a7f68eda21
Joe Dodgson <j.dodgson@live.co.uk>
2020-08-25 11:21:47 +0100

    Amended server.js to not serve frontend files to user

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const path = require("path");
 
 // Set up express server
 const app = express();
@@ -11,16 +10,6 @@ const PORT = process.env.PORT || 3001;
 // Set up middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  // Set static build folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 // Add API routes and view
 app.use(routes);
